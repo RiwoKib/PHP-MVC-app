5197d450ed81f11e48841a22bd2a6a4d12097c2f
Riwokib <riwellkib@gmail.com>
2023-07-17 20:06:48 +0300

    refactoring

--- public/index.php
+++ public/index.php
@@ -1,9 +1,26 @@
-<?php
+<?php  
+   function loadController($controller){
+    switch ($controller) {
+        case 'users':
+            $strFileController = '../app/controllers/User.php';
+            require_once $strFileController;
+            $controllerObj = new UserController();
+            break;
+        default:
+            $strFileController = '../app/controllers/User.php';
+            require_once $strFileController;
+            $controllerObj = new UserController();
+            break;
+    }
+    return $controllerObj;
+}
 
-require_once '../app/init.php';
-
-
-$app = new APP();
- 
-?>
+function launchAction($controllerObj){
+    if(isset($_GET["action"])){
+        $controllerObj->run($_GET["action"]);
+    } else {
+        $controllerObj->run(DEFAULT_ACTION);
+    }
+}
+?>  
 
