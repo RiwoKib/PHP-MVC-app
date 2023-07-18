2b98f3b1727a6514b90a992dda4564373fb24c5d
Riwokib <riwellkib@gmail.com>
2023-07-18 16:51:18 +0300

    index.php initialize the app

--- public/index.php
+++ public/index.php
@@ -1,26 +1,8 @@
-<?php  
-   function loadController($controller){
-    switch ($controller) {
-        case 'users':
-            $strFileController = '../app/controllers/User.php';
-            require_once $strFileController;
-            $controllerObj = new UserController();
-            break;
-        default:
-            $strFileController = '../app/controllers/User.php';
-            require_once $strFileController;
-            $controllerObj = new UserController();
-            break;
-    }
-    return $controllerObj;
-}
+<?php
 
-function launchAction($controllerObj){
-    if(isset($_GET["action"])){
-        $controllerObj->run($_GET["action"]);
-    } else {
-        $controllerObj->run(DEFAULT_ACTION);
-    }
-}
-?>  
+
+require '../app/core/Autoload.php';
+
+
+$app = new App();   
 
