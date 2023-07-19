5afd81496f18d0941ab284c6c7baea79c8fa38fc
Riwokib <riwellkib@gmail.com>
2023-07-19 14:28:31 +0300

    navbar and sidebar view

--- app/views/includes/navbar.view.php
+++ app/views/includes/navbar.view.php
@@ -1,47 +1,196 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container">
-    <a class="navbar-brand" href="index.php">Bilkens Management</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-      
-      
-      <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href=" ">Store</a>
-      </li>
+<div class="main-wrapper">
 
-      <li class="nav-item">
-         <a class="nav-link" href=" ">Categories</a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link" href=" ">Cart</a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link" href=" ">My Sales</a>
-      </li>
- 
-       
-      <li class="nav-item">
-          <a class="nav-link" href=" ">Register</a>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link" href=" ">Login</a>
-      </li>
-      <li class="nav-item dropdown">
-          
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            riwell
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="">Logout</a></li>
-          </ul>
-        </li> 
-      </ul>
+  <div class="header">
+
+    <div class="header-left active">
+      <a href="index.html" class="logo">
+        <img src="<?=ASSETS?>/img/logo.png" alt="">
+      </a>
+    <a href="index.html" class="logo-small">
+      <img src="<?=ASSETS?>/img/logo-small.png" alt="">
+    </a>
+    <a id="toggle_btn" href="javascript:void(0);">
+    </a>
+  </div>
+
+  <a id="mobile_btn" class="mobile_btn" href="#sidebar">
+    <span class="bar-icon">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+  </a>
+
+  <ul class="nav user-menu">
+    
+    <li class="nav-item">
+      <div class="top-nav-search">
+        <a href="javascript:void(0);" class="responsive-search">
+          <i class="fa fa-search"></i>
+        </a>
+        <form action="#">
+          <div class="searchinputs">
+            <input type="text" placeholder="Search Here ...">
+            <div class="search-addon">
+              <span><img src="<?=ASSETS?>/img/icons/closes.svg" alt="img"></span>
+            </div>
+          </div>
+          <a class="btn" id="searchdiv"><img src="<?=ASSETS?>/img/icons/search.svg" alt="img"></a>
+        </form>
+      </div>
+    </li>
+
+
+    <!-- <li class="nav-item dropdown has-arrow flag-nav">
+    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="javascript:void(0);" role="button">
+    <img src="<?=ASSETS?>/img/flags/us1.png" alt="" height="20">
+    </a>
+    <div class="dropdown-menu dropdown-menu-right">
+    <a href="javascript:void(0);" class="dropdown-item">
+    <img src="<?=ASSETS?>/img/flags/us.png" alt="" height="16"> English
+    </a>
+    <a href="javascript:void(0);" class="dropdown-item">
+    <img src="<?=ASSETS?>/img/flags/fr.png" alt="" height="16"> French
+    </a>
+    <a href="javascript:void(0);" class="dropdown-item">
+    <img src="<?=ASSETS?>/img/flags/es.png" alt="" height="16"> Spanish
+    </a>
+    <a href="javascript:void(0);" class="dropdown-item">
+    <img src="<?=ASSETS?>/img/flags/de.png" alt="" height="16"> German
+    </a>
+    </div>
+    </li> -->
+
+
+    <li class="nav-item dropdown">
+      <a href="javascript:void(0);" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
+        <img src="<?=ASSETS?>/img/icons/notification-bing.svg" alt="img"> <span class="badge rounded-pill">4</span>
+      </a>
+      <div class="dropdown-menu notifications">
+        <div class="topnav-dropdown-header">
+          <span class="notification-title">Notifications</span>
+          <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+        </div>
+        
+        <div class="noti-content"> 
+          <ul class="notification-list">
+
+              <li class="notification-message">
+                <a href="activities.html">
+                <div class="media d-flex">
+                  <span class="avatar flex-shrink-0">
+                    <img alt="" src="<?=ASSETS?>/img/profiles/avatar-03.jpg">
+                  </span>
+                  <div class="media-body flex-grow-1">
+                    <p class="noti-details"><span class="noti-title">Tarah Shropshire</span> changed the task name <span class="noti-title">Appointment booking with payment gateway</span></p>
+                    <p class="noti-time"><span class="notification-time">6 mins ago</span></p>
+                  </div>
+                </div>
+                </a>
+              </li>
+
+              <li class="notification-message">
+                <a href="activities.html">
+                <div class="media d-flex">
+                  <span class="avatar flex-shrink-0">
+                    <img alt="" src="<?=ASSETS?>/img/profiles/avatar-03.jpg">
+                  </span>
+                  <div class="media-body flex-grow-1">
+                    <p class="noti-details"><span class="noti-title">Tarah Shropshire</span> changed the task name <span class="noti-title">Appointment booking with payment gateway</span></p>
+                    <p class="noti-time"><span class="notification-time">6 mins ago</span></p>
+                  </div>
+                </div>
+                </a>
+              </li>
+
+              <li class="notification-message">
+                <a href="activities.html">
+                <div class="media d-flex">
+                  <span class="avatar flex-shrink-0">
+                    <img alt="" src="<?=ASSETS?>/img/profiles/avatar-06.jpg">
+                  </span>
+                  <div class="media-body flex-grow-1">
+                    <p class="noti-details"><span class="noti-title">Misty Tison</span> added <span class="noti-title">Domenic Houston</span> and <span class="noti-title">Claire Mapes</span> to project <span class="noti-title">Doctor available module</span></p>
+                    <p class="noti-time"><span class="notification-time">8 mins ago</span></p>
+                  </div>
+                </div>
+                </a>
+              </li>
+
+              <li class="notification-message">
+                <a href="activities.html">
+                <div class="media d-flex">
+                  <span class="avatar flex-shrink-0">
+                    <img alt="" src="<?=ASSETS?>/img/profiles/avatar-17.jpg">
+                  </span>
+                  <div class="media-body flex-grow-1">
+                    <p class="noti-details"><span class="noti-title">Rolland Webber</span> completed task 
+                    <span class="noti-title">Patient and Doctor video conferencing</span>
+                    </p>
+                    <p class="noti-time"><span class="notification-time">12 mins ago</span></p>
+                  </div>
+                </div>
+                </a>
+              </li>
+
+              <li class="notification-message">
+                <a href="activities.html">
+                <div class="media d-flex">
+                  <span class="avatar flex-shrink-0">
+                    <img alt="" src="<?=ASSETS?>/img/profiles/avatar-13.jpg">
+                  </span>
+                  <div class="media-body flex-grow-1">
+                    <p class="noti-details"><span class="noti-title">Bernardo Galaviz</span> added new task <span class="noti-title">Private chat module</span></p>
+                    <p class="noti-time"><span class="notification-time">2 days ago</span></p>
+                  </div>
+                </div>
+                </a>
+              </li>
+
+          </ul> 
+        </div>
+
+        <div class="topnav-dropdown-footer">
+          <a href="activities.html">View all Notifications</a>
+        </div>
+      </div>
+    </li>
+
+    <li class="nav-item dropdown has-arrow main-drop">
+      <a href="javascript:void(0);" class="dropdown-toggle nav-link userset" data-bs-toggle="dropdown">
+        <span class="user-img"><img src="<?=ASSETS?>/img/profiles/avator1.jpg" alt="">
+          <span class="status online"></span>
+        </span>
+      </a>
+
+      <div class="dropdown-menu menu-drop-user">
+        <div class="profilename">
+          <div class="profileset">
+            <span class="user-img"><img src="<?=ASSETS?>/img/profiles/avator1.jpg" alt="">
+            <span class="status online"></span></span>
+            <div class="profilesets">
+              <h6>John Doe</h6>
+              <h5>Admin</h5>
+            </div>
+          </div>
+          <hr class="m-0">
+          <a class="dropdown-item" href="profile.html"> <i class="me-2" data-feather="user"></i> My Profile</a>
+          <a class="dropdown-item" href="generalsettings.html"><i class="me-2" data-feather="settings"></i>Settings</a>
+          <hr class="m-0">
+          <a class="dropdown-item logout pb-0" href="signin.html"><img src="<?=ASSETS?>/img/icons/log-out.svg" class="me-2" alt="img">Logout</a>
+        </div>
+      </div>
+    </li>
+  </ul>
+
+
+  <div class="dropdown mobile-user-menu">
+    <a href="javascript:void(0);" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
+    <div class="dropdown-menu dropdown-menu-right">
+      <a class="dropdown-item" href="profile.html">My Profile</a>
+      <a class="dropdown-item" href="generalsettings.html">Settings</a>
+      <a class="dropdown-item" href="signin.html">Logout</a>
     </div>
   </div>
-</nav>
+
+</div>
---
+++ app/views/includes/sidebar.view.php
@@ -0,0 +1,164 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li>
+                <a href=""><img src="<?=ASSETS?>/img/icons/dashboard.svg" alt="img"><span> Dashboard</span> </a>
+                </li>
+
+                <li class="submenu">
+                    <a href="javascript:void(0);">
+                        <img src="<?=ASSETS?>/img/icons/product.svg" alt="img"><span> Product</span> 
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul>
+                        <li><a href=" ">Product List</a></li>
+                        <li><a href=" ">Add Product</a></li>
+                        <li><a href=" ">Category List</a></li>
+                        <li><a href=" ">Add Category</a></li>
+                        <li><a href=" ">Sub Category List</a></li>
+                        <li><a href=" ">Add Sub Category</a></li>
+                        <li><a href=" ">Brand List</a></li>
+                        <li><a href=" ">Add Brand</a></li>
+                        <li><a href=" ">Import Products</a></li>
+                        <li><a href="" class="active">Print Barcode</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="javascript:void(0);">
+                        <img src="<?=ASSETS?>/img/icons/sales1.svg" alt="img"><span> Sales</span> 
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul>
+                        <li><a href="">Sales List</a></li>
+                        <li><a href="">POS</a></li>
+                        <li><a href="">New Sales</a></li>
+                        <li><a href="">Sales Return List</a></li>
+                        <li><a href="">New Sales Return</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                <a href="javascript:void(0);">
+                    <img src="<?=ASSETS?>/img/icons/purchase1.svg" alt="img"><span> Purchase</span> 
+                    <span class="menu-arrow"></span>
+                </a>
+                <ul>
+                    <li><a href="">Purchase List</a></li>
+                    <li><a href="">Add Purchase</a></li>
+                    <li><a href="">Import Purchase</a></li>
+                </ul>
+                </li>
+
+                <li class="submenu">
+                <a href="javascript:void(0);">
+                    <img src="<?=ASSETS?>/img/icons/expense1.svg" alt="img"><span> Expense</span> 
+                    <span class="menu-arrow"></span>
+                </a>
+                <ul>
+                    <li><a href="">Expense List</a></li>
+                    <li><a href="">Add Expense</a></li>
+                    <li><a href="">Expense Category</a></li>
+                </ul>
+                </li>
+
+                <li class="submenu">
+                <a href="javascript:void(0);">
+                    <img src="<?=ASSETS?>/img/icons/quotation1.svg" alt="img"><span> Quotation</span> 
+                    <span class="menu-arrow"></span>
+                </a>
+                <ul>
+                    <li><a href="">Quotation List</a></li>
+                    <li><a href="">Add Quotation</a></li>
+                </ul>
+                </li>
+
+                <li class="submenu">
+                <a href="javascript:void(0);">
+                    <img src="<?=ASSETS?>/img/icons/transfer1.svg" alt="img"><span> Transfer</span> 
+                    <span class="menu-arrow"></span>
+                </a>
+                <ul>
+                    <li><a href="">Transfer List</a></li>
+                    <li><a href="">Add Transfer </a></li>
+                    <li><a href="">Import Transfer </a></li>
+                </ul>
+                </li>
+
+                <li class="submenu">
+                <a href="javascript:void(0);">
+                    <img src="<?=ASSETS?>/img/icons/return1.svg" alt="img"><span> Return</span> 
+                    <span class="menu-arrow"></span>
+                </a>
+                <ul>
+                    <li><a href="">Sales Return List</a></li>
+                    <li><a href="">Add Sales Return </a></li>
+                    <li><a href="">Purchase Return List</a></li>
+                    <li><a href="">Add Purchase Return </a></li>
+                </ul>
+                </li>
+
+                <li class="submenu">
+                <a href="javascript:void(0);">
+                    <img src="<?=ASSETS?>/img/icons/users1.svg" alt="img"><span> People</span> 
+                    <span class="menu-arrow"></span></a>
+                <ul>
+                    <li><a href="">Customer List</a></li>
+                    <li><a href="">Add Customer </a></li>
+                    <li><a href="">Supplier List</a></li>
+                    <li><a href="">Add Supplier </a></li>
+                    <li><a href="">User List</a></li>
+                    <li><a href="">Add User</a></li>
+                    <li><a href="">Store List</a></li>
+                    <li><a href="">Add Store</a></li>
+                </ul>
+                </li>
+                
+        
+        
+        
+                <li class="submenu">
+                    <a href="javascript:void(0);">
+                        <img src="<?=ASSETS?>/img/icons/time.svg" alt="img">
+                        <span> Report</span> <span class="menu-arrow"></span>
+                    </a>
+                    <ul>
+                        <li><a href="">Purchase order report</a></li>
+                        <li><a href="">Inventory Report</a></li>
+                        <li><a href="">Sales Report</a></li>
+                        <li><a href="">Invoice Report</a></li>
+                        <li><a href="">Purchase Report</a></li>
+                        <li><a href="">Supplier Report</a></li>
+                        <li><a href="">Customer Report</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="javascript:void(0);">
+                        <img src="<?=ASSETS?>/img/icons/users1.svg" alt="img">
+                        <span> Users</span> <span class="menu-arrow"></span>
+                    </a>
+                    <ul>
+                        <li><a href="">New User </a></li>
+                        <li><a href="">Users List</a></li>
+                    </ul>
+                </li>
+
+                <li class="submenu">
+                    <a href="javascript:void(0);">
+                        <img src="<?=ASSETS?>/img/icons/settings.svg" alt="img"><span> Settings</span> <span class="menu-arrow"></span>
+                    </a>
+                    <ul>
+                        <li><a href="">General Settings</a></li>
+                        <li><a href="">Email Settings</a></li>
+                        <li><a href="">Payment Settings</a></li>
+                        <li><a href="">Currency Settings</a></li>
+                        <li><a href="">Group Permissions</a></li>
+                        <li><a href="">Tax Rates</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
