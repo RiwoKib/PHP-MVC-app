29c9141e934830a67975829bba3c73bdd0e7a7d8
Riwokib <riwellkib@gmail.com>
2023-07-19 14:28:11 +0300

    title change

--- app/views/includes/header.view.php
+++ app/views/includes/header.view.php
@@ -1,31 +1,25 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    
+    <meta charset="utf-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0"> 
     
-    <title>Inventory Management</title>
+    <title>Bilkens Inventory Management</title> 
 
-
-    <link rel="shortcut icon" type="image/x-icon" href="<?=ASSETS?>/img/favicon.jpg">
-
-    <link rel="stylesheet" href="<?=ASSETS?>/css/bootstrap.min.css">
-
-    <link rel="stylesheet" href="<?=ASSETS?>/css/animate.css">
-
-    <link rel="stylesheet" href="<?=ASSETS?>/plugins/select2/css/select2.min.css">
-
-    <link rel="stylesheet" href="<?=ASSETS?>/plugins/owlcarousel/owl.carousel.min.css">
-
-    <link rel="stylesheet" href="<?=ASSETS?>/css/dataTables.bootstrap4.min.css">
-
+    <!--Bootstrap template css links-->
+    
+    <link rel="shortcut icon" type="image/x-icon" href="<?=ASSETS?>/img/favicon.jpg"> 
+    <link rel="stylesheet" href="<?=ASSETS?>/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="<?=ASSETS?>/css/animate.css"> 
+    <link rel="stylesheet" href="<?=ASSETS?>/plugins/select2/css/select2.min.css"> 
+    <link rel="stylesheet" href="<?=ASSETS?>/plugins/owlcarousel/owl.carousel.min.css"> 
+    <link rel="stylesheet" href="<?=ASSETS?>/css/dataTables.bootstrap4.min.css"> 
     <link rel="stylesheet" href="<?=ASSETS?>/plugins/fontawesome/css/fontawesome.min.css">
-    <link rel="stylesheet" href="<?=ASSETS?>/plugins/fontawesome/css/all.min.css">
-
+    <link rel="stylesheet" href="<?=ASSETS?>/plugins/fontawesome/css/all.min.css"> 
+    
     <link rel="stylesheet" href="<?=ASSETS?>/css/style.css">
-    
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<!--     Bootstrap cdn link
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> -->
+ 
   </head>
   <body>
