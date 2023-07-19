4d22c45a420eeacfb9e5ee44385fea06ce983831
Riwokib <riwellkib@gmail.com>
2023-07-19 14:27:53 +0300

    footer js links

--- app/views/includes/footer.view.php
+++ app/views/includes/footer.view.php
@@ -1,3 +1,19 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+
+
+<!--Js links-->
+
+<script src="<?=ASSETS?>/js/jquery-3.6.0.min.js"></script> 
+<script src="<?=ASSETS?>/js/feather.min.js"></script> 
+<script src="<?=ASSETS?>/js/jquery.slimscroll.min.js"></script> 
+<script src="<?=ASSETS?>/js/bootstrap.bundle.min.js"></script> 
+<script src="<?=ASSETS?>/plugins/owlcarousel/owl.carousel.min.js"></script> 
+<script src="<?=ASSETS?>/plugins/select2/js/select2.min.js"></script> 
+<script src="<?=ASSETS?>/js/script.js"></script>
+
+
+<!-- Js Bootstrap cdn
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script> -->
+
   </body>
 </html>
