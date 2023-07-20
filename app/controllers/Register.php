46773b8c418673b1e8a2a7009e5e8d5b73354b38
Riwokib <riwellkib@gmail.com>
2023-07-21 00:06:39 +0300

    Register controller done

--- app/controllers/Register.php
+++ app/controllers/Register.php
@@ -5,6 +5,43 @@ class Register extends Controller
 {
     function index()
 	{ 
-		 echo $this->view('register');
+		$errors = array();
+
+		if(isset($_POST['registerBtn'])){ 
+			$data = array(
+                'firstname' => $_POST['firstname'],
+                'lastname' => $_POST['lastname'],
+                'email' => $_POST['email'],
+                'phone_number' => $_POST['phone'],
+                'password' => $_POST['password'],
+                'confirm' => $_POST['confirm'], 
+                'rank' => $_POST['rank'],
+            );
+		
+		
+			  // Instantiate the User model
+			  $userModel = new User();
+
+			  if ($userModel->validate($data)) { 
+
+				unset($data['confirm']);
+				$user_id = $userModel->makeUserID();
+
+				$data['user_id'] = $user_id;
+
+				if ($userModel->insert($data)) { 
+					$this->redirect('login');
+					// echo "User registered successfully!";
+				} else { 
+					echo "Error: Unable to register the user.";
+				}
+			} else { 
+				$errors = $userModel->errors;
+				// print_r($errors);
+			}
+		  
+		}
+
+		 $this->view('register',['errors' => $errors]);
 	}
 }
