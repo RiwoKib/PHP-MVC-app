d103a10e88e2fb86507585833197ac3cdd3649cd
Riwokib <riwellkib@gmail.com>
2023-07-21 00:08:11 +0300

    Authentication Class

--- app/models/Authenticate.php
+++ app/models/Authenticate.php
@@ -1,1 +1,55 @@
 
+<?php
+
+/**
+ * Authentication class
+ */
+class Auth
+{
+	
+	public static function authenticate($user)
+	{ 
+		$_SESSION['USER'] = $user;
+	}
+
+	public static function logout()
+	{ 
+		if(isset($_SESSION['USER']))
+		{
+			unset($_SESSION['USER']);
+		}
+	}
+
+	public static function logged_in()
+	{ 
+		if(isset($_SESSION['USER']))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	public static function user()
+	{
+		if(isset($_SESSION['USER']))
+		{
+			return $_SESSION['USER']->firstname;
+		}
+
+		return false;
+	}
+
+	public static function __callStatic($method,$params)
+	{
+		$prop = strtolower(str_replace("get","",$method));
+
+		if(isset($_SESSION['USER']->$prop))
+		{
+			return $_SESSION['USER']->$prop;
+		}
+
+		return 'Unknown';
+	}
+
+}
