66afd33e311e452d897b0cfe0ce2281da549fde1
Riwokib <riwellkib@gmail.com>
2023-07-22 13:56:18 +0300

    Categories/Products use global imageuploader

--- app/controllers/Categories.php
+++ app/controllers/Categories.php
@@ -42,9 +42,9 @@ class Categories extends Controller
 		
 			if($add->validate($data))
 			{
-				$imagePath = $this->handleImageUpload();
+				$imagePath = handleImageUpload();
  
-               $data['image'] = basename($imagePath);
+               $data['image'] = $imagePath;
 
 				if($add->insert($data))
 				{
@@ -61,40 +61,7 @@ class Categories extends Controller
 
 		$this->view('category.add', ['errors' => $errors]);
 	}
-
-	private function handleImageUpload()
-    {
-        $uploadDir = UPLOADS . "/";
-        $allowedExtensions = array('jpg', 'jpeg', 'png', 'gif');
-
-		if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
-			$tmpName = $_FILES['image']['tmp_name'];
-			$originalName = $_FILES['image']['name'];
-			$extension = strtolower(pathinfo($originalName, PATHINFO_EXTENSION));
-	
-			// Check if the uploaded file has an allowed extension
-			if (!in_array($extension, $allowedExtensions)) {
-				echo "Error: Unsupported file type.";
-				return null;
-			}
-	
-			// Generate a unique filename to avoid overwriting existing files
-			$uniqueName = uniqid('image_', true) . '.' . $extension;
-			$destination = $uploadDir . $uniqueName;
-	
-			// Move the uploaded file to the desired location
-			if (move_uploaded_file($tmpName, $destination)) {
-				echo "Uploaded image destination: " . $destination . "<br>";
-				return $destination;
-			} else {
-				echo "Error: Unable to move uploaded file.";
-				return null;
-			}
-		} else {
-			echo "Error uploading the image.";
-			return null;
-		}
-    }
+ 
 
 
 }
--- app/controllers/Products.php
+++ app/controllers/Products.php
@@ -57,7 +57,7 @@ class Products extends Controller
 				$data['tax'] = floatval($_POST['tax']) / 100;
 				$data['discount'] = floatval($_POST['discount']) / 100;
 
-				$imagePath = $this->handleImageUpload();
+				$imagePath = handleImageUpload();
  
                	$data['image'] = basename($imagePath);
 
@@ -76,38 +76,5 @@ class Products extends Controller
 
 		$this->view('product.add', ['errors' => $errors]);
 	}
-
-	private function handleImageUpload()
-    {
-        $uploadDir = UPLOADS . "/";
-        $allowedExtensions = array('jpg', 'jpeg', 'png', 'gif');
-
-		if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
-			$tmpName = $_FILES['image']['tmp_name'];
-			$originalName = $_FILES['image']['name'];
-			$extension = strtolower(pathinfo($originalName, PATHINFO_EXTENSION));
-	
-			// Check if the uploaded file has an allowed extension
-			if (!in_array($extension, $allowedExtensions)) {
-				echo "Error: Unsupported file type.";
-				return null;
-			}
-	
-			// Generate a unique filename to avoid overwriting existing files
-			$uniqueName = uniqid('image_', true) . '.' . $extension;
-			$destination = $uploadDir . $uniqueName;
-	
-			// Move the uploaded file to the desired location
-			if (move_uploaded_file($tmpName, $destination)) {
-				echo "Uploaded image destination: " . $destination . "<br>";
-				return $destination;
-			} else {
-				echo "Error: Unable to move uploaded file.";
-				return null;
-			}
-		} else {
-			echo "Error uploading the image.";
-			return null;
-		}
-    }
+ 
 }
