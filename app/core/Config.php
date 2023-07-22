0cbe54cb153817629c348f7d3e679b330b8db7ff
Riwokib <riwellkib@gmail.com>
2023-07-22 03:04:48 +0300

    define uploads path

--- app/core/Config.php
+++ app/core/Config.php
@@ -2,6 +2,9 @@
 
 define('ROOT', 'http://localhost/PHP-MVC/public');
 define('ASSETS', 'http://localhost/PHP-MVC/assets');
+define('UPLOADS', 'C:/xampp/htdocs/PHP-MVC/uploads'); 
+define('UPLOADED', 'http://localhost/PHP-MVC/uploads');
+
 
 define("DB_HOST", 'localhost');  
 define("DB_USER", 'root');  
