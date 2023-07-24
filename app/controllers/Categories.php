3291c0c258981e43ea2a35ab49634c3d04e93bbe
Riwokib <riwellkib@gmail.com>
2023-07-25 01:25:48 +0300

    edit method category controller

--- app/controllers/Categories.php
+++ app/controllers/Categories.php
@@ -62,6 +62,49 @@ class Categories extends Controller
 		$this->view('category.add', ['errors' => $errors]);
 	}
  
+	function edit($id = null)
+	{			
+		$errors = array();
 
+		if(!Authenticate::logged_in())
+		{
+			$this->redirect('login');
+		}
+		
+		$category = new Category();	 
+		$data = $category->where('id',$id);	 
+		
+		if(isset($_POST['updateCategory']))
+		{
+			$data = array(
+                'category_name' => $_POST['name'], 
+                'sku' => $_POST['sku'],
+                'description' => $_POST['desc'], 
+            );	  	 
+			
+			if($category->validate($data))
+			{ 
+				
+				$imagePath = handleImageUpload();
+
+				$data['image'] = $imagePath;
+				
+				if($category->update($id, $data))
+				{
+					$this->redirect('categories');
+				}else{
+					echo "Unable to update category". $category->getErrorMessage();
+				}
+				}else{
+				$errors = $category->errors;
+			}
+
+			
+		} 
+		
+		$this->view('category.update', [	'row' => $data, 	
+											'errors' => $errors]);
+
+	}
 
 }
