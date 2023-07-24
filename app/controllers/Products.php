d942eb66e516cfee1fe07c1497d0e22fa1da7a57
Riwokib <riwellkib@gmail.com>
2023-07-25 01:25:33 +0300

    edit method product controller

--- app/controllers/Products.php
+++ app/controllers/Products.php
@@ -76,5 +76,65 @@ class Products extends Controller
 
 		$this->view('product.add', ['errors' => $errors]);
 	}
+
+	function edit($id = null)
+	{			
+		$errors = array();
+
+		if(!Authenticate::logged_in())
+		{
+			$this->redirect('login');
+		}
+		
+		$product = new Product();	 
+		$data = $product->where('id',$id);	 
+		
+		if(isset($_POST['updateProduct']))
+		{
+			$data = array(
+                'product_name' => $_POST['name'], 
+                'sku' => $_POST['sku'],
+                'tax' => $_POST['tax'], 
+                'status' => $_POST['status'], 
+                'sub_category' => $_POST['sub'],
+                'unit' => $_POST['unit'], 
+                'category_ID' => $_POST['cat'], 
+                'product_quantity' => $_POST['qty'],
+                'discount' => $_POST['discount'], 
+                'price' => $_POST['price'],
+                'brand' => $_POST['brand'],
+                'description' => $_POST['desc'],
+            ); 	 
+			
+			if($product->validate($data))
+			{ 
+				
+
+				$data['product_quantity'] = intval($_POST['qty']);
+				$data['price'] = intval($_POST['price']);
+				$data['tax'] = floatval($_POST['tax']) / 100;
+				$data['discount'] = floatval($_POST['discount']) / 100;
+				
+				$imagePath = handleImageUpload();
+
+				$data['image'] = $imagePath;
+				
+				if($product->update($id, $data))
+				{
+					$this->redirect('products');
+				}else{
+					echo "Unable to update product". $product->getErrorMessage();
+				}
+				}else{
+				$errors = $product->errors;
+			}
+
+			
+		} 
+		
+		$this->view('product.update', [	'row' => $data, 	
+											'errors' => $errors]);
+
+	}
  
 }
