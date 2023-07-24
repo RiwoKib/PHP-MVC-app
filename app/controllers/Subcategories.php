a46d430502de6081e721371e4fa79a82cb26a7fc
Riwokib <riwellkib@gmail.com>
2023-07-25 01:26:12 +0300

    subcategory edit/add/delete methods

--- app/controllers/Subcategories.php
+++ app/controllers/Subcategories.php
@@ -63,4 +63,71 @@ class Subcategories extends Controller
 		$this->view('subcategory.add', ['rows' => $data,
 										'errors' => $errors]);
 	}
+
+	function edit($id = null)
+	{			
+		$errors = array();
+
+		if(!Authenticate::logged_in())
+		{
+			$this->redirect('login');
+		}
+		
+		$subcat = new SubCategory();	 
+		$data = $subcat->where('id',$id);	 
+		
+		if(isset($_POST['updateSubCategory']))
+		{
+			$data = array(
+				'parent_category' => $_POST['parent'],
+                'category_name' => $_POST['name'], 
+                'sku' => $_POST['sku'],
+                'description' => $_POST['desc'], 
+            );	  	 
+			
+			if($subcat->validate($data))
+			{ 
+
+				if($subcat->update($id, $data))
+				{
+					$this->redirect('subcategories');
+				}else{
+					echo "Unable to update subcategory". $subcat->getErrorMessage();
+				}
+				}else{
+				$errors = $subcat->errors;
+			}
+
+			
+		}
+		
+		$parent = new Category();	 
+		$parentCategory = $parent->findAll();
+		
+		$this->view('subcategory.update', [	'row' => $data,
+											'categories' => $parentCategory,	
+											'errors' => $errors]);
+
+	}
+
+	function delete($id= null)
+	{
+		if(!Authenticate::logged_in())
+		{
+			$this->redirect('login');
+		}
+
+		$errors = array();
+
+		$subcat = new SubCategory();
+
+		if(isset($_POST['deleteBtn']))
+		{
+			$subcat->delete($id);
+			$this->redirect('subcategories');
+		}else{
+			echo $subcat->getErrorMessage();
+		}
+		
+	}
 }
