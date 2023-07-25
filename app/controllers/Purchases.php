ba20d22c5307343be047399275f85b027c4e2f37
Riwokib <riwellkib@gmail.com>
2023-07-25 03:57:07 +0300

    purchases controller

---
+++ app/controllers/Purchases.php
@@ -0,0 +1,34 @@
+<?php
+
+
+/**
+ * home controller
+ */
+class Purchases extends Controller
+{
+	
+	function index()
+	{	
+
+		if(!Authenticate::logged_in())
+		{
+			$this->redirect('login');
+		}
+
+		$purchase = new Purchase();
+
+		$data = $purchase->findAll();
+
+		$this->view('purchases', ['rows' => $data]);
+	}
+
+    function add()
+    {
+        $this->view('purchases.add');
+    }
+
+    function import()
+    {
+        $this->view('purchases.import');
+    }
+}
