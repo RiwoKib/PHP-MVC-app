d10d95b8bbc66d18088274094fb57a108c910f7a
Riwokib <riwellkib@gmail.com>
2023-07-26 17:15:20 +0300

    Purchase return model

---
+++ app/models/PurchaseReturn.php
@@ -0,0 +1,14 @@
+<?php
+
+
+/***
+ * PurchaseReturn Model
+ */
+
+ class PurchaseReturn extends Model
+ {
+    public function __construct()
+    {   
+        parent::__construct('purchase_returns');
+    }
+}
