dfb0aa6e0fa84ff7afcd0053d887eeb6f1fc8796
Riwokib <riwellkib@gmail.com>
2023-07-31 21:02:03 +0300

    check empty field validation

--- app/models/Category.php
+++ app/models/Category.php
@@ -20,22 +20,18 @@
         //check input fields
 
        
-        if (!preg_match('/^[a-zA-Z ]+$/', $DATA['category_name'])) {
-            $this->errors['name'] = "Only letters are allowed in category name";
-        }
+        // if (!preg_match('/^[a-zA-Z0-9]+$/', $DATA['category_name'])) {
+        //     $this->errors['name'] = "Only letters/numbers are allowed in category name";
+        // }
         
         if(empty($DATA['category_name']))
         {
             $this->errors['name'] = "Fill category name";
         }
-        if(!preg_match('/^[a-zA-Z0-9]+$/', $DATA['sku']))
-        {
-            $this->errors['sku'] = "Letters followed by numbers in sku";
-        }
-        if(empty($DATA['sku']))
-        {
-            $this->errors['sku'] = "Fill SKU (category code) e.g CT002";
-        }
+        // if(!preg_match('/^[a-zA-Z0-9]+$/', $DATA['category_ID']))
+        // {
+        //     $this->errors['category_ID'] = "Letters followed by numbers in category_ID";
+        // }
 
         if(empty($DATA['description']))
         {
