6f8c60ebce82e7000c8339e021b20fb51a164dbc
Riwokib <riwellkib@gmail.com>
2023-08-01 23:29:24 +0300

    sales add view search for products

--- app/views/sales.add.view.php
+++ app/views/sales.add.view.php
@@ -4,202 +4,172 @@
 
 <div class="page-wrapper">
     <div class="content">
-    <div class="page-header">
-    <div class="page-title">
-    <h4>Add Sale</h4>
-    <h6>Add your new sale</h6>
-    </div>
-    </div>
-    <div class="card">
-    <div class="card-body">
-    <div class="row">
-    <div class="col-lg-3 col-sm-6 col-12">
-    <div class="form-group">
-    <label>Customer</label>
-    <div class="row">
-    <div class="col-lg-10 col-sm-10 col-10">
-    <select class="select">
-    <option>Choose</option>
-    <option>Customer Name</option>
-    </select>
-    </div>
-    <div class="col-lg-2 col-sm-2 col-2 ps-0">
-    <div class="add-icon">
-    <span><img src="<?=ASSETS?>/img/icons/plus1.svg" alt="img"></span>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    <div class="col-lg-3 col-sm-6 col-12">
-    <div class="form-group">
-    <label>Customer</label>
-    <div class="input-groupicon">
-    <input type="text" placeholder="Choose Date" class="datetimepicker">
-    <a class="addonset">
-    <img src="<?=ASSETS?>/img/icons/calendars.svg" alt="img">
-    </a>
-    </div>
-    </div>
-    </div>
-    <div class="col-lg-3 col-sm-6 col-12">
-    <div class="form-group">
-    <label>Supplier</label>
-    <select class="select">
-    <option>Choose</option>
-    <option>Supplier Name</option>
-    </select>
-    </div>
-    </div>
-    <div class="col-lg-12 col-sm-6 col-12">
-    <div class="form-group">
-    <label>Product Name</label>
-    <div class="input-groupicon">
-    <input type="text" placeholder="Please type product code and select...">
-    <div class="addonset">
-    <img src="<?=ASSETS?>/img/icons/scanners.svg" alt="img">
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    <div class="row">
-    <div class="table-responsive mb-3">
-    <table class="table">
-    <thead>
-    <tr>
-    <th>#</th>
-    <th>Product Name</th>
-    <th>QTY</th>
-    <th>Price</th>
-    <th>Discount</th>
-    <th>Tax</th>
-    <th>Subtotal</th>
-    <th></th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-    <td>1</td>
-    <td class="productimgname">
-    <a class="product-img">
-    <img src="<?=ASSETS?>/img/product/product7.jpg" alt="product">
-    </a>
-    <a href="javascript:void(0);">Apple Earpods</a>
-    </td>
-    <td>1.00</td>
-    <td>15000.00</td>
-    <td>0.00</td>
-    <td>0.00</td>
-    <td>1500.00</td>
-    <td>
-    <a href="javascript:void(0);" class="delete-set"><img src="<?=ASSETS?>/img/icons/delete.svg" alt="svg"></a>
-    </td>
-    </tr>
-    <tr>
-    <td>2</td>
-    <td class="productimgname">
-    <a class="product-img">
-    <img src="<?=ASSETS?>/img/product/product8.jpg" alt="product">
-    </a>
-    <a href="javascript:void(0);">iPhone 11</a>
-    </td>
-    <td>1.00</td>
-    <td>1500.00</td>
-    <td>0.00</td>
-    <td>0.00</td>
-    <td>1500.00</td>
-    <td>
-    <a href="javascript:void(0);" class="delete-set"><img src="<?=ASSETS?>/img/icons/delete.svg" alt="svg"></a>
-    </td>
-    </tr>
-    <tr>
-    <td>2</td>
-    <td class="productimgname">
-    <a class="product-img">
-    <img src="<?=ASSETS?>/img/product/product1.jpg" alt="product">
-    </a>
-    <a href="javascript:void(0);">Macbook pro</a>
-    </td>
-    <td>1.00</td>
-    <td>1500.00</td>
-    <td>0.00</td>
-    <td>0.00</td>
-    <td>1500.00</td>
-    <td>
-    <a href="javascript:void(0);" class="delete-set"><img src="<?=ASSETS?>/img/icons/delete.svg" alt="svg"></a>
-    </td>
-    </tr>
-    </tbody>
-    </table>
-    </div>
-    </div>
-    <div class="row">
-    <div class="col-lg-3 col-sm-6 col-12">
-    <div class="form-group">
-    <label>Order Tax</label>
-    <input type="text">
-    </div>
-    </div>
-    <div class="col-lg-3 col-sm-6 col-12">
-    <div class="form-group">
-    <label>Discount</label>
-    <input type="text">
-    </div>
-    </div>
-    <div class="col-lg-3 col-sm-6 col-12">
-    <div class="form-group">
-    <label>Shipping</label>
-    <input type="text">
-    </div>
-    </div>
-    <div class="col-lg-3 col-sm-6 col-12">
-    <div class="form-group">
-    <label>Status</label>
-    <select class="select">
-    <option>Choose Status</option>
-    <option>Completed</option>
-    <option>Inprogress</option>
-    </select>
-    </div>
-    </div>
-    <div class="row">
-    <div class="col-lg-6 ">
-    <div class="total-order w-100 max-widthauto m-auto mb-4">
-    <ul>
-    <li>
-    <h4>Order Tax</h4>
-    <h5>$ 0.00 (0.00%)</h5>
-    </li>
-    <li>
-    <h4>Discount	</h4>
-    <h5>$ 0.00</h5>
-    </li>
-    </ul>
-    </div>
-    </div>
-    <div class="col-lg-6 ">
-    <div class="total-order w-100 max-widthauto m-auto mb-4">
-    <ul>
-    <li>
-    <h4>Shipping</h4>
-    <h5>$ 0.00</h5>
-    </li>
-    <li class="total">
-    <h4>Grand Total</h4>
-    <h5>$ 0.00</h5>
-    </li>
-    </ul>
-    </div>
-    </div>
-    </div>
-    <div class="col-lg-12">
-    <a href="javascript:void(0);" class="btn btn-submit me-2">Submit</a>
-    <a href="javascript:void(0);" class="btn btn-cancel">Cancel</a>
-    </div>
-    </div>
-    </div>
-    </div>
+        <div class="page-header">
+            <div class="page-title">
+                <h4>Add Sale</h4>
+                <h6>Add your new sale</h6>
+            </div>
+        </div>
+
+        
+        
+        <form action="" method="post">
+            <div class="col-lg-6 col-sm-6 col-12">
+                <div class="form-group">
+                    <label>Search Product Name</label>
+                    <div class="input-groupicon">
+                        <input type="text" id="searchInput" name="searchData" placeholder="Please type product name/category code and select...">
+                        <div class="addonset">
+                            <button style="background:none; border:none" name="searchProduct"><img src="<?=ASSETS?>/img/icons/search.svg" alt=""></button>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        </form>  
+
+        <div class="card">
+            <div class="card-body">
+                <form action="" method="post">
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Customer</label>
+                                <div class="row">
+                                    <div class="col-lg-10 col-sm-10 col-10">
+                                        <select name="customer" class="select">
+                                            <option value="">Choose Customer</option>
+                                            <option value="Walk-in-Customer">Walk-in-Customer</option>
+                                            <?php foreach ($customers as $customer){?>
+                                                <option value="<?=$customer->firstname?> <?=$customer->lastname?>"><?=$customer->firstname?> <?=$customer->lastname?></option>
+                                            <?php }?>
+                                        </select>
+                                    </div> 
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+
+                    <div class="row">
+                        <div class="table-responsive mb-3">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                        <label class="checkboxs">
+                                        <input type="checkbox" id="select-all">
+                                        <span class="checkmarks"></span>
+                                        </label>
+                                        </th> 
+                                        <th>Product Name</th>
+                                        <th>QTY</th>
+                                        <th>Price</th>
+                                        <th>Discount</th>
+                                        <th>Tax</th>
+                                        <th>Subtotal</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php if(isset($results) && $results){?>
+                                        <?php foreach($results as $product){?>
+                                            <tr> 
+
+                                                <td>
+                                                    <label class="checkboxs">
+                                                        <input type="checkbox">
+                                                        <span class="checkmarks"></span>
+                                                    </label>
+                                                </td>   
+                                                <td class="productimgname">
+                                                    <a class="product-img">
+                                                        <img src="<?=UPLOADED?>/<?=$product->image?>" alt="product">
+                                                    </a>
+                                                    <a href="javascript:void(0);"><?=$product->product_name?></a>
+                                                </td>
+                                                <td><?=$product->product_quantity?></td>
+                                                <td><?=$product->selling_price?></td>
+                                                <td><?=$product->discount?></td>
+                                                <td><?=$product->tax?></td>
+                                                <td><?=$product->sub_total?></td>
+                                                <td>
+                                                    <a href="javascript:void(0);" class="delete-set"><img src="<?=ASSETS?>/img/icons/delete.svg" alt="svg"></a>
+                                                </td>
+                                            </tr>
+                                        <?php }?>
+                                    <?php }else{?>
+                                        <div>
+                                            <p class="text-info">** Search for Products Above **</p>
+                                        </div>
+                                    <?php }?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Order Tax</label>
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Discount</label>
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Shipping</label>
+                                <input type="text">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Status</label>
+                                <select class="select">
+                                    <option>Choose Status</option>
+                                    <option>Completed</option>
+                                    <option>Inprogress</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row "> 
+                        <div class="col-lg-12 float-md-right">
+                            <div class="total-order ">
+                                <ul>
+                                    <li>
+                                        <h4>Order Tax</h4>
+                                        <h5 class="text-success">KES 0.00 (0.00%)</h5>
+                                    </li>
+                                    <li>
+                                        <h4>Discount	</h4>
+                                        <h5 class="text-success">KES 0.00</h5>
+                                    </li>
+                                    <li>
+                                        <h4>Shipping</h4>
+                                        <h5 class="text-success">KES 0.00</h5>
+                                    </li>
+                                    <li class="total">
+                                        <h4>Grand Total</h4>
+                                        <h5 class="text-success">KES 0.00</h5>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12">
+                            <button type="submite" name="addSale" class="btn btn-submit me-2">Submit</button>
+                            <a href="<?=ROOT?>/sales" class="btn btn-cancel">Cancel</a>
+                        </div>
+                        
+                    </div> 
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 
