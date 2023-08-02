a2c63a3ac646e7fbb5ad8e5591a1cd03542476c5
Riwokib <riwellkib@gmail.com>
2023-08-02 03:28:23 +0300

    search for products

--- app/controllers/SalesReturn.php
+++ app/controllers/SalesReturn.php
@@ -24,6 +24,32 @@ class SalesReturn extends Controller
 
     function add()
     {
-        $this->view('salesreturn.add');
+		$errors = array();
+		$results = false;
+
+		if(!Authenticate::logged_in())
+		{
+			$this->redirect('login');
+		}
+
+		
+		if(isset($_POST['searchData']))
+		{	
+			$sale = new Sale();
+
+			$searchData = '%'.trim($_POST['searchData']).'%';
+ 
+			$query = "SELECT * FROM products WHERE product_name LIKE ? OR category_ID LIKE ?";
+			$results = $sale->conn->query($query, [$searchData,$searchData]); 
+		}
+
+		
+
+		$customer = new Customer();
+		$customers = $customer->findAll();
+
+        $this->view('salesReturn.add', ['errors' => $errors, 
+										'results' => $results,
+								  		'customers' => $customers,	]);
     }
 }
