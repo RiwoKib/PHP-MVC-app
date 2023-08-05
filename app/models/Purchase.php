c86eb9f7ae9f69833b33e90863cc3ea63ea37ce8
Riwokib <riwellkib@gmail.com>
2023-08-05 17:25:21 +0300

    purchase model validateimported

--- app/models/Purchase.php
+++ app/models/Purchase.php
@@ -12,4 +12,32 @@
         parent::__construct('purchases');
     }
 
-}
+
+    public function validateImported($DATA)
+    {
+        $this->errors = array();    
+
+        //check input fields 
+        if(empty($DATA['supplier_code']) || empty($DATA['category_ID']) || empty($DATA['product_ID']) || empty($DATA['status']))
+        {
+            $this->errors['fields'] = "Crosscheck the excel table with the required fields";
+        }
+        // if (!preg_match('/^[a-zA-Z0-9 ]+$/', $DATA['product_name'])) {
+        //     $this->errors['name'] = "Only letters/numbers are allowed in product name";
+        // }
+        // if(!preg_match('/^[a-zA-Z0-9]+$/', $DATA['sku']))
+        // {
+        //     $this->errors['sku'] = "Letters followed by numbers in sku";
+        // } 
+
+        
+         
+ 
+        if(count($this->errors) == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+ }
