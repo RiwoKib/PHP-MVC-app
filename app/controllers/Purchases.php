9c9c8b1a27e07e6b3b62e99499064b83085c75e6
Riwokib <riwellkib@gmail.com>
2023-08-05 17:26:36 +0300

    purchase controller  handle import

--- app/controllers/Purchases.php
+++ app/controllers/Purchases.php
@@ -16,6 +16,9 @@ class Purchases extends Controller
 		}
 
 		$purchase = new Purchase();
+		$supplier = new Supplier();
+
+		// $supplier_name = $supplier->where('supplier_name', '')
 
 		$data = $purchase->findAll();
 
@@ -53,10 +56,69 @@ class Purchases extends Controller
 								  		'suppliers' => $suppliers,	]);
     }
 
-    function import()
-    {
-        $this->view('purchases.import');
-    }
+	function import()
+	{
+		$errors = array();
+
+		if(!Authenticate::logged_in())
+		{
+			$this->redirect('login');
+		}
+
+		if(isset($_POST['importPurchases']))
+		{
+			$file = $_FILES['purchases_import']['tmp_name'];
+
+			$data = extractDataFromExcel($file);
+
+			$purchase = new Purchase(); 
+
+			foreach ($data as $row)
+			{	
+				$insertData = array(
+				'supplier_code' => $row[0],
+				'product_ID' => $row[1],
+				'category_ID' => $row[2],
+				'tax' => $row[3],
+				'discount' => $row[5],
+				'shipping' => $row[10],
+				'status' => $row[4],
+				'description' => $row[11],
+				'total' => $row[6],
+				'paid' => $row[7],
+				'due' => $row[8],
+				'payment_status' => $row[9], 
+				);  
+
+				// show( $insertData);
+
+				if($purchase->validateImported($insertData))
+				{ 
+					$insertData['tax'] = floatval($insertData['tax']) / 100;
+					$insertData['discount'] = floatval($insertData['discount']) / 100;
+
+					$insertData['purchase_code'] = makeCode('purchases');
+
+					if($purchase->insert($insertData))
+					{
+						
+						echo "inserted successfully";
+					}else{
+						echo $purchase->getErrorMessage();
+					}
+				}else{
+					$errors = $purchase->errors;
+				}
+
+
+			}
+
+			// $this->redirect('purchases');
+			
+		}
+
+		$this->view('purchases.import', ['errors' => $errors]);
+	}
 
 	function report()
 	{
