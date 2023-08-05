e8a27974bc1a952593faf9a1e6b1c5705c52f713
Riwokib <riwellkib@gmail.com>
2023-08-05 13:04:44 +0300

    Transfer controller search products

--- app/controllers/Transfers.php
+++ app/controllers/Transfers.php
@@ -24,7 +24,33 @@ class Transfers extends Controller
 
     function add()
     {
-        $this->view('transfers.add');
+		$errors = array();
+		$results = false;
+
+		if(!Authenticate::logged_in())
+		{
+			$this->redirect('login');
+		}
+
+		if(isset($_POST['searchProduct']))
+		{	
+			$sale = new Sale();
+
+			if($_POST['searchData'] != "")
+			{
+				$searchData = '%'.trim($_POST['searchData']).'%';
+ 
+				$query = "SELECT * FROM products WHERE product_name LIKE ? OR category_ID LIKE ?";
+				$results = $sale->conn->query($query, [$searchData,$searchData]); 
+		
+			}else{
+				$errors[] = 'Type something to search';
+			}
+		}
+
+
+        $this->view('transfers.add', ['errors' => $errors,
+									  'results' => $results]);
     }
 
     function import()
