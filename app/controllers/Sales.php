6c804b045b069f874aadb3f0e891c9454d9d4b4c
Riwokib <riwellkib@gmail.com>
2023-08-09 15:25:28 +0300

    remove handle search/ save select

--- app/controllers/Sales.php
+++ app/controllers/Sales.php
@@ -26,68 +26,17 @@ class Sales extends Controller
     function add()
     {	
 		$errors = array();
-		$results = false;
-		$insertData = array();
 
 		if(!Authenticate::logged_in())
 		{
 			$this->redirect('login');
-		}
-
-		
-		if(isset($_POST['searchProduct']))
-		{	
-			$sale = new Sale();
-
-			if($_POST['searchData'] != "")
-			{
-				$searchData = '%'.trim($_POST['searchData']).'%';
- 
-				$query = "SELECT * FROM products WHERE product_name LIKE ? OR category_ID LIKE ?";
-				$results = $sale->conn->query($query, [$searchData,$searchData]); 
-		
-			}else{
-				$errors[] = 'Type something to search';
-			}
-
-		}
-
-		if(isset($_POST['save_selected']))
-		{	 
-
-			$product = new Product();
-			$checked = $_POST['selected'];  	 
-
-			foreach ($checked as $prodID)
-			{	
-				$productRow = $product->where('product_ID', $prodID);
-
-				$grabPrice = array(
-					'id' => $productRow[0]->product_ID,
-					'price' => $productRow[0]->selling_price,  
-					'product_name' => $productRow[0]->product_name,
-					'image'=> $productRow[0]->image,
-					'tax' => $productRow[0]->tax,
-					'discount' => $productRow[0]->discount,
-					'unit' => $productRow[0]->unit
-				); 
-
-				$InsertData[] = $grabPrice;
-				// show($insertData);
-			}
-
-			
-		}
-
-		
+		}  		
 
 		$customer = new Customer();
 		$customers = $customer->findAll();
 
-        $this->view('sales.add', [	'errors' => $errors, 
-									'results' => $results,
-								  	'customers' => $customers,
-									'selected' => $InsertData]);
+        $this->view('sales.add', [	'errors' => $errors,
+								  	'customers' => $customers]);
     }
 
 
