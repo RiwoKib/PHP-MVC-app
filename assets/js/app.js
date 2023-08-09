7305f1d564d99be579f5f5117b4368e235d920d2
Riwokib <riwellkib@gmail.com>
2023-08-09 23:21:19 +0300

    increment decrement btn changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,10 @@
-$(document).ready(function (){
-
-    const saveSelectedBtn = document.getElementById('saveSelectedBtn');
-    const checkboxes = document.querySelectorAll('.product-checkbox');
-
-    // Add a change event listener to each checkbox
-    checkboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('change', function() {
-            // Check if at least one checkbox is checked
-            const atLeastOneChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
-
-            
-            if (atLeastOneChecked) {
-                saveSelectedBtn.style.display = 'block';
-            } else {
-                saveSelectedBtn.style.display = 'none';
-            }
-        });
-    });
-
+$(document).ready(function (){ 
     
-    //increment && decrement  button
-  $(document).on('click', '.incrementBtn', function (e) {    
+    
+    $(document).on('click', '.incrementBtn', function (e) {    
         e.preventDefault();
         // alert('increment')
-        var qty = $(this).closest('.table').find('.input-qty').val();
+        var qty = $(this).closest('.product_data').find('.input-qty').val();
         var value = parseInt(qty, 10);
         value = isNaN(value) ? 0 : value;
 
@@ -40,7 +21,7 @@ $(document).ready(function (){
         e.preventDefault();
 
         // alert('decrement')
-        var qty = $(this).closest('.table').find('.input-qty').val();
+        var qty = $(this).closest('.product_data').find('.input-qty').val();
         var value = parseInt(qty, 10);
         value = isNaN(value) ? 0 : value;
 
@@ -50,8 +31,4 @@ $(document).ready(function (){
             $(this).closest('.product_data').find('.input-qty').val(value);
         }
     });
-
-
- 
-
 })
