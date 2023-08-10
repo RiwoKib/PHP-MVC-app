351a549044d9f00922bb9717c997f0d7ccd1dcbe
Riwokib <riwellkib@gmail.com>
2023-08-10 20:42:26 +0300

    quoteitems model

---
+++ app/models/QuoteItems.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/***
+ * QuoteItems Model
+ */
+
+ class QuoteItems extends Model
+ {
+    public function __construct()
+    {   
+        parent::__construct('quote_items');
+    }
+
+}
