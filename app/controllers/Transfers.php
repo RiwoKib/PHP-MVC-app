11008961514bf50dcbe42e1d5fff32a003ecf229
Riwokib <riwellkib@gmail.com>
2023-08-13 14:50:40 +0300

    remove searchproducts

--- app/controllers/Transfers.php
+++ app/controllers/Transfers.php
@@ -25,32 +25,14 @@ class Transfers extends Controller
     function add()
     {
 		$errors = array();
-		$results = false;
 
 		if(!Authenticate::logged_in())
 		{
 			$this->redirect('login');
 		}
 
-		if(isset($_POST['searchProduct']))
-		{	
-			$sale = new Sale();
 
-			if($_POST['searchData'] != "")
-			{
-				$searchData = '%'.trim($_POST['searchData']).'%';
- 
-				$query = "SELECT * FROM products WHERE product_name LIKE ? OR category_ID LIKE ?";
-				$results = $sale->conn->query($query, [$searchData,$searchData]); 
-		
-			}else{
-				$errors[] = 'Type something to search';
-			}
-		}
-
-
-        $this->view('transfers.add', ['errors' => $errors,
-									  'results' => $results]);
+        $this->view('transfers.add', ['errors' => $errors,]);
     }
 
     function import()
