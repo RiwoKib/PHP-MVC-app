834a068ff9faad2d3d0c41a1a7bec6fdbe3b8e42
Riwokib <riwellkib@gmail.com>
2023-08-13 14:46:30 +0300

    success message

--- app/views/purchases.add.view.php
+++ app/views/purchases.add.view.php
@@ -183,7 +183,7 @@
         </div>
 
         <div id="successMessage" style="display: none" class="alert alert-success alert-dismissible fade show" role="alert">
-            <strong>Success!</strong> New Quote Added.
+            <strong>Success!</strong> New Purchase Added.
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     </div>
