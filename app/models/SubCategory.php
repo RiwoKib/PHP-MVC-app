904c0c4a9d422541821608dc9caff700dbe15b1b
Riwokib <riwellkib@gmail.com>
2023-08-22 23:04:45 +0300

    validate input

--- app/models/SubCategory.php
+++ app/models/SubCategory.php
@@ -18,20 +18,19 @@
         $this->errors = array();    
 
         //check input fields
-
-       
-        if (!preg_match('/^[a-zA-Z ]+$/', $DATA['category_name'])) {
-            $this->errors['name'] = "Only letters are allowed in category name";
+        if(empty($DATA['category_name'])){
+            $this->errors['category_name'] = "** Fill Category Name **";
+        }else if (!preg_match('/^[a-zA-Z ]+$/', $DATA['category_name'])) {
+            $this->errors['category_name'] = "** No Special Characters allowed in category name **";
         }
-        
-        if(empty($DATA['category_name']) || empty($DATA['parent_category']))
-        {
-            $this->errors['name'] = "Fill Both categories";
+
+        if(empty($DATA['parent_category'])){
+            $this->errors['parent_category'] = "** Select Parent Category **";
         }
 
         if(empty($DATA['description']))
         {
-            $this->errors['desc'] = "Description field must be filled";
+            $this->errors['description'] = "** Description field must be filled **";
         }
          
  
