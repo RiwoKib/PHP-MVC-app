f702e1f4c36d0680671712c6ed8f5b6a03ef4073
Riwokib <riwellkib@gmail.com>
2023-09-06 22:51:47 +0300

    match subcategory.add view input fields

--- app/views/subcategory.update.view.php
+++ app/views/subcategory.update.view.php
@@ -11,49 +11,44 @@
             </div>
         </div>
 
-        <?php if(count($errors) > 0):?>
-            <div class="alert alert-warning alert-dismissible fade show p-1 mx-2" role="alert">
-            <strong class="text-danger">Error!</strong>
-                <?php foreach($errors as $error):?>
-                <br><?=$error?>
-            <?php endforeach;?>
-            <span  type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </span>
-            </div>
-        <?php endif;?>
-
         <div class="card">
             <div class="card-body">
                 <form action="" method="post">
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Parent Category</label>
+                                    <select class="select" name="parent">
+                                        <option value="0">Choose Category</option>
+                                        <?php foreach ($categories as $parentCategory) {?>
+                                                <option value="<?=$parentCategory->category_name?>"><?=$parentCategory->category_name?></option>
+                                            <?php }?>
+                                    </select>
+                                    <?php if(isset($errors['parent_category'])){?>
+                                        <span class="text-danger"><?=$errors['parent_category']?></span>
+                                    <?php }?>
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+                                <div class="form-group">
+                                    <label>Category Name</label>
+                                    <input value="<?=$row[0]->category_name?>" name="name" type="text">
+                                    <?php if(isset($errors['category_name'])){?>
+                                        <span class="text-danger"><?=$errors['category_name']?></span>
+                                    <?php }?>
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
                     <div class="row">
-                        <div class="col-lg-4 col-sm-6 col-12">
-                            <div class="form-group">
-                                <label>Parent Category</label>
-                                <select class="select" name="parent">
-                                    <option value="">Choose Category</option>
-                                    <?php foreach ($categories as $parent) {?>
-                                            <option value="<?=$parent->category_name?>"><?=$parent->category_name?></option>
-                                        <?php }?>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-sm-6 col-12">
-                            <div class="form-group">
-                                <label>Category Name</label>
-                                <input value="<?=$row[0]->category_name?>" name="name" type="text">
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-sm-6 col-12">
-                            <div class="form-group">
-                                <label>Category Code</label>
-                                <input value="<?=$row[0]->sku?>" name="sku" type="text">
-                            </div>
-                        </div>
-                        <div class="col-lg-12">
+                        <div class="col-lg-6">
                             <div class="form-group">
                                 <label>Description</label>
                                 <textarea name="desc" class="form-control"><?=$row[0]->description?></textarea>
+                                <?php if(isset($errors['description'])){?>
+                                    <span class="text-danger"><?=$errors['description']?></span>
+                                <?php }?>
                             </div>
                         </div>
                         <div class="col-lg-12">
